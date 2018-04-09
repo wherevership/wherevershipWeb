daa8a5d2ef2da41f15ebc50e2fb1ba996601ba9b
micropop <lcg1989@hotmail.com>
2018-04-09 18:10:50 +0800

    9/4 update

--- application/views/frontEnd/dt_cost_preview.php
+++ application/views/frontEnd/dt_cost_preview.php
@@ -2,85 +2,73 @@
 			<div class="container">
 		
 				<form method="POST" action="<?=base_url('dtc_shipper')?>" class="form-horizontal">
-					<div class="row">
-						<h1>Cost Info</h1>
-						<div class="col-xs-12 col-md-12 col-lg-12" style="padding: 10px 30px;">
-								<h2>Cost</h2>
-								<div class="form-group">
-									<label for="cost"><span class="glyphicon glyphicon-info-sign"></span> Cost(RM)</label>
-									<input type="hidden" class="form-control" id="cost" name="cost" value="123" />
-									<div class="alert alert-info">
-									<p id="cost1"></p>
-									</div>
-								</div>
-						</div>
-					</div>
-					<div class="row">
-						
-						<div class="col-xs-12 col-md-6" style="padding: 10px 30px;">
-							<h2>Product Dimension</h2>
-									<div class="form-group">
-										<label for="wt"><span class="glyphicon glyphicon-info-sign"></span> Weight(kg)</label>
-										<input type="hidden" id="wt" name="weight" value="<?=$weight?>" />
-										<p class="form-control"><?=$weight?></p>
-									</div>
-									<div class="form-group">
-										<label for="l"><span class="glyphicon glyphicon-info-sign"></span> Length(cm)</label>
-										<input type="hidden" id="l" name="length" value="<?=$length?>"/>
-										<p class="form-control"><?=$length?></p>
-									</div>
-									<div class="form-group">
-										<label for="w"><span class="glyphicon glyphicon-info-sign"></span> width(cm)</label>
-										<input type="hidden" id="w" name="width" value="<?=$width?>"/>
-										<p class="form-control"><?=$width?></p>
-									</div>
-									<div class="form-group">
-										<label for="h"><span class="glyphicon glyphicon-info-sign"></span> height(cm)</label>
-										<input type="hidden" id="h" name="height" value="<?=$height?>"/>
-										<p class="form-control"><?=$height?></p>
-									</div>
-									<div class="form-group">
-										<label for="vwt"><span class="glyphicon glyphicon-info-sign"></span> Volumetic Weight(kg)</label>
-										<input type="hidden" id="vwt" name="v_weight" value="<?=$v_weight?>"/>
-										<p class="form-control"><?=$v_weight?></p>
-									</div>
-						</div>
-			
-			
-						<div class="col-xs-12 col-md-6" style="padding: 10px 30px;">
-							<h2>Address</h2>
-								<div class="form-group">
-									<label for="frm"><span class="glyphicon glyphicon-info-sign"></span> From</label>
-									<input type="hidden" class="form-control" id="frm" name="fromState" value="<?=$fromState?>"/>
-									<p class="form-control"><?=$fromState?></p>
-									<p class="form-control"><?=$fromStateZone?></p>
-								</div>
-								<div class="form-group">
-									<label for="to"><span class="glyphicon glyphicon-info-sign"></span> To</label>
-									<input type="hidden" id="to" name="toState" value="<?=$toState?>"/>
-									<p class="form-control"><?=$toState?></p>
-									<p class="form-control"><?=$toStateZone?></p>
-								</div>
-							
-						</div>
-					</div>
-					<div class="row">
-						
+					<input type="hidden" id="wt" name="weight" value="<?=$weight?>" />
+					<input type="hidden" id="l" name="length" value="<?=$length?>"/>
+					<input type="hidden" id="w" name="width" value="<?=$width?>"/>
+					<input type="hidden" id="h" name="height" value="<?=$height?>"/>
+					<input type="hidden" id="vwt" name="v_weight" value="<?=$v_weight?>"/>
+					<input type="hidden" id="frm" name="fromState" value="<?=$fromState?>"/>
+					<input type="hidden" id="frm" name="fromStateZone" value="<?=$fromStateZone?>"/>
+					<input type="hidden" id="frm" name="toStateZone" value="<?=$toStateZone?>"/>
+					<input type="hidden" id="to" name="toState" value="<?=$toState?>"/>
+					<input type="hidden" id="cost1" name="cost" value=""/>
 					
-					</div>
+					<div class="space">
+						<div class="row">
+							<div class="panel panel-default">
+								<div class="panel panel-title-bar">
+									<img src="<?=base_url('assets2/image/poslaju.png')?>" width="20%">
+								</div>
+								<div class="panel-body">
+									<div class="col-md-3">
+										<h3>From:</h3>
+										<p><?=$fromState?></p>
+										<p><?=$fromStateZone?> Malaysia</p>
+									</div>
+									<div class="col-md-3">
+										<h3>To:</h3>
+										<p><?=$toState?></p>
+										<p><?=$toStateZone?> Malaysia</p>
+									</div>
+									<div class="col-md-3">
+										<p>
+											Length: <?=$length?>cm<br>
+											Width: <?=$width?>cm<br>
+											Height: <?=$height?>cm<br>
+											Weight: <?=$weight?>Kg<br>
+											Volumetic Weight: <?=$v_weight?>Kg
+										
+										</p>
+									</div>
+									<div class="col-md-3">
+										<div>
+											<h3>Price</h3>
+										
+											<p style="color: #00a9b0;"><b>RM<span id="cost"></span></b></p>
+										</div>
+										<div class="form-group">
+											<input type="submit" value="Continue >>" id="next" class="form-control btn btn-success"/>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					
+						
 					<div class="row">
 						<div class="form-group">
-					<div class="col-md-6">
-						<a type="button" class="form-control btn" href="javascript:history.go(-1)">Cancel</a>
-					</div>
+							<div class="col-md-6">
+								<a type="button" class="form-control btn" href="javascript:history.go(-1)">Cancel</a>
+							</div>
 				
-					<div class="col-md-6">
-						<input type="submit" value="Continue >>" id="next" class="form-control btn btn-success"/>
-					
+							<div class="col-md-6">
+							</div>
+						</div>
 					</div>
-				</div>
 				</div>
 				</form>
+				
+				
        
 		</div>
 	</div>
@@ -96,9 +84,12 @@
 			var keys = Object.keys(object1);
 			for (var i=0; i<keys.length; i++) {
 				var k= keys[i];
+				console.log(k);
 				if (weight >= object1[k].minWeight && weight <= object1[k].maxWeight && formStateZone == object1[k].fromStateZone && toStateZone == object1[k].toStateZone ) {
-					$("#cost1").text(object1[k].cost.toFixed(2));
-					$("#cost").val(object1[k].cost.toFixed(2));
+					
+					$("#cost").text(object1[k].cost.toFixed(2));
+					$("#cost1").val(object1[k].cost.toFixed(2));
+					console.log(object1[k].cost.toFixed(2));
 				}
 			  	
 			}
